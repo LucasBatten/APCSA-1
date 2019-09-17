d798ef4312ac57cd0fd2d3a80c20b2848554c611
Dennis Burton <dburton@careevolution.com>
2019-09-17 17:29:45 -0400

    Add outer for loop

---
+++ ProjectOne.java
@@ -0,0 +1,16 @@
+public class ProjectOne {
+   public static void main( String[] args ){
+      // print the line
+      for( int i = 6; i >=0; i-- ) {
+         System.out.print( "line: " + i ); // we will remove this later, it helps keep track of where we are for debugging
+         // print the beginning stars
+         // print the beginning spaces
+         // print the slashes
+         // print the backslashes
+         // print the ending spaces
+         // print the ending stars
+         
+         System.out.println();
+      }
+   }
+}
