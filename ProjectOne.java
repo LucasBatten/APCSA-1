9449847c8cae8b52abbd701ec3cabd5496c72222
Dennis Burton <dburton@careevolution.com>
2019-09-17 17:47:49 -0400

    add spaces

--- ProjectOne.java
+++ ProjectOne.java
@@ -1,10 +1,19 @@
 public class ProjectOne {
+   public static int MAX_LINE_INDEX = 6;
    public static void main( String[] args ){
       // print the line
-      for( int i = 6; i >=0; i-- ) {
+      for( int i = MAX_LINE_INDEX; i >= 0; i-- ) {
          System.out.print( "line: " + i ); // we will remove this later, it helps keep track of where we are for debugging
+         
          // print the beginning stars
+         for( int j = i; j > 0; j-- ){
+            System.out.print( "*" );
+         }
+         
          // print the beginning spaces
+         for( int j = 1; j < (MAX_LINE_INDEX-i); j++ ){
+            System.out.print( "-" );
+         }
          // print the slashes
          // print the backslashes
          // print the ending spaces
