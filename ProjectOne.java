64ce902ff27634ed6e4d3f0e1fb1b21838c15514
Dennis Burton <dburton@careevolution.com>
2019-09-17 22:31:28 -0400

    wrapping up Project 1

--- ProjectOne.java
+++ ProjectOne.java
@@ -1,11 +1,10 @@
 public class ProjectOne {
    public static int MAX_LINE_INDEX = 6;
    public static int SPACES_STARS_LENGTH = 7;
+   public static int SLASH_BACKSLASH_LENGTH = 12;
    public static void main( String[] args ){
       // print the line
-      for( int i = MAX_LINE_INDEX; i >= 0; i-- ) {
-         System.out.print( "line: " + i ); // we will remove this later, it helps keep track of where we are for debugging
-         
+      for( int i = MAX_LINE_INDEX; i >= 0; i-- ) {         
          // print the beginning stars
          for( int j = i; j > 0; j-- ){
             System.out.print( "*" );
@@ -13,13 +12,29 @@ public class ProjectOne {
          
          // print the beginning spaces
          for( int j = 0; j < (SPACES_STARS_LENGTH - i); j++ ){
-            System.out.print( "-" );
+            System.out.print( " " );
          }
-         // print the slashes
-         // print the backslashes
-         // print the ending spaces
-         // print the ending stars
          
+         // print the slashes
+         for( int j = 0; j < (i*2); j++ ){
+            System.out.print("/");
+         }
+         
+         // print the backslashes
+         for( int j = 0; j < SLASH_BACKSLASH_LENGTH - (i*2); j++ ){
+            System.out.print("\\");
+         } 
+         
+         // print the ending spaces
+         for( int j = 0; j < (SPACES_STARS_LENGTH - i); j++ ){
+            System.out.print( " " );
+         }
+                  
+         // print the ending stars
+         for( int j = i; j > 0; j-- ){
+            System.out.print( "*" );
+         }
+                  
          System.out.println();
       }
    }
